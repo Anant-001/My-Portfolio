c68edb5f82bf2461ddcd672e73b0aae1670a3ce7
Anant-001 <anantdevliyal01@gmail.com>
2026-05-18 18:58:41 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,285 @@
+const navLinks = [
+    {
+        name: "Work",
+        link: "#work",
+    },
+    {
+        name: "Experience",
+        link: "#experience",
+    },
+    {
+        name: "Skills",
+        link: "#skills",
+    },
+    {
+        name: "Testimonials",
+        link: "#testimonials",
+    },
+];
+
+const words = [
+    { text: "Ideas", imgPath: "/images/ideas.svg" },
+    { text: "Concepts", imgPath: "/images/concepts.svg" },
+    { text: "Designs", imgPath: "/images/designs.svg" },
+    { text: "Code", imgPath: "/images/code.svg" },
+    { text: "Ideas", imgPath: "/images/ideas.svg" },
+    { text: "Concepts", imgPath: "/images/concepts.svg" },
+    { text: "Designs", imgPath: "/images/designs.svg" },
+    { text: "Code", imgPath: "/images/code.svg" },
+];
+
+const counterItems = [
+    { value: 0, suffix: "+", label: "Years of Experience" },
+    { value: 25, suffix: "+", label: "Projects in Ui/Ux " },
+    { value: 18, suffix: "+", label: " Projects" },
+    { value: 90, suffix: "%", label: "Completed Project Rate" },
+];
+
+const logoIconsList = [
+    {
+        imgPath: "/images/logos/company-logo-1.png",
+    },
+    {
+        imgPath: "/images/logos/company-logo-2.png",
+    },
+    {
+        imgPath: "/images/logos/company-logo-3.png",
+    },
+    {
+        imgPath: "/images/logos/company-logo-4.png",
+    },
+    {
+        imgPath: "/images/logos/company-logo-5.png",
+    },
+    {
+        imgPath: "/images/logos/company-logo-6.png",
+    },
+    {
+        imgPath: "/images/logos/company-logo-7.png",
+    },
+    {
+        imgPath: "/images/logos/company-logo-8.png",
+    },
+    {
+        imgPath: "/images/logos/company-logo-9.png",
+    },
+    {
+        imgPath: "/images/logos/company-logo-10.png",
+    },
+    {
+        imgPath: "/images/logos/company-logo-11.png",
+    },
+];
+
+const abilities = [
+    {
+        imgPath: "/images/seo.png",
+        title: "Quality Focus",
+        desc: "Delivering high-quality results while maintaining attention to every detail.",
+    },
+    {
+        imgPath: "/images/chat.png",
+        title: "Reliable Communication",
+        desc: "Keeping you updated at every step to ensure transparency and clarity.",
+    },
+    {
+        imgPath: "/images/time.png",
+        title: "On-Time Delivery",
+        desc: "Making sure projects are completed on schedule, with quality & attention to detail.",
+    },
+];
+
+const techStackImgs = [
+    {
+        name: "React Developer",
+        imgPath: "/images/logos/react.png",
+    },
+    {
+        name: "Python Developer",
+        imgPath: "/images/logos/python.svg",
+    },
+    {
+        name: "Backend Developer",
+        imgPath: "/images/logos/node.png",
+    },
+    {
+        name: "Interactive Developer",
+        imgPath: "/images/logos/three.png",
+    },
+    {
+        name: "Project Manager",
+        imgPath: "/images/logos/git.svg",
+    },
+];
+
+const techStackIcons = [
+    {
+        name: "React Developer",
+        modelPath: "/models/react_logo-transformed.glb",
+        scale: 1,
+        rotation: [0, 0, 0],
+    },
+    {
+        name: "Figma Developer",
+        modelPath: "/models/figma.fbx.glb",
+        scale: 2.5,
+        rotation: [0, 0, 0],
+    },
+    {
+        name: "Front end Developer",
+        modelPath: "/models/node-transformed.glb",
+        scale: 5,
+        rotation: [0, -Math.PI / 2, 0],
+    },
+    {
+        name: "Interactive Developer",
+        modelPath: "/models/three.js-transformed.glb",
+        scale: 0.05,
+        rotation: [0, 0, 0],
+    },
+    {
+        name: "UI/UX Designer",
+        modelPath: "/models/ui_ux_typing_machine.glb",
+        scale: 1.5,
+        rotation: [0, -Math.PI / 4, 0],
+    },
+];
+
+const expCards = [
+    {
+        review:
+            "Anant brought creativity and technical expertise to the team, significantly improving our frontend performance. His work has been invaluable in delivering faster experiences.",
+        imgPath: "/images/exp1.png",
+        logoPath: "/images/logo1.png",
+        title: "Frontend Developer",
+        date: "July 2025 - Present",
+        responsibilities: [
+            "Developed and maintained user-friendly showcase in the Vercel website.",
+            "A Good UI/UX designers for your Web development to ensure seamless user experiences.",
+            "Optimized web applications for maximum speed and scalability.",
+        ],
+    },
+    {
+        review:
+            "Anant contributions to Docker's web applications have been outstanding. He approaches challenges with a problem-solving mindset.",
+        imgPath: "/images/exp2.png",
+        logoPath: "/images/logo2.png",
+        title: "Full Stack Developer",
+        date: "June 2025 - August 2025",
+        responsibilities: [
+            "Led the development of Docker's web applications, focusing on scalability.",
+            "Worked with backend engineers to integrate APIs seamlessly with the frontend.",
+            "Contributed to open-source projects that were used with the Docker ecosystem.",
+        ],
+    },
+    {
+        review:
+            "Anant work with Ui/Ux to design web and  brought a high level of quality and efficiency. He delivered solutions that enhanced our web experience & meet our product goals.",
+        imgPath: "/Showcase/Figma-logo.png",
+        logoPath: "/Showcase/Figma.png",
+        title: "User Interface / User Experience Designer",
+        date: "March 2025 - May 2025",
+        responsibilities: [
+            "Built and  Design goodlooking UI/UX for the web application.",
+            "Improved web performance and user experience through code optimization and testing.",
+            "Use different Platforms as well as Tools like Fremer, Adobe XD, etc to improvise productivity to implement features based on feedback.",
+        ],
+    },
+];
+
+const expLogos = [
+    {
+        name: "logo1",
+        imgPath: "/images/logo1.png",
+    },
+    {
+        name: "logo2",
+        imgPath: "/images/logo2.png",
+    },
+    {
+        name: "logo3",
+        imgPath: "/images/logo3.png",
+    },
+];
+
+const testimonials = [
+    {
+        name: "Esther Howard",
+        mentions: "@estherhoward",
+        review:
+            "Anant’s grasp on UI/UX is exceptional. He took our vision and turned it into an interface that’s not only visually stunning but also incredibly intuitive. Every design choice felt purposeful and enhanced the user journey. He’s a rare talent in user-centered design.",
+        imgPath: "/images/client1.png",
+    },
+    {
+        name: "Wade Warren",
+        mentions: "@wadewarren",
+        review:
+            "If you're looking for someone who lives and breathes frontend development, Anant is your guy. The way he brought our designs to life with pixel-perfect precision and smooth animations was just incredible. Our users immediately noticed the difference.",
+        imgPath: "/images/client3.png",
+    },
+    {
+        name: "Guy Hawkins",
+        mentions: "@guyhawkins",
+        review:
+            "Collaborating with Anant was a game-changer. From UI/UX planning to final deployment, he handled every aspect of web development with confidence and expertise. The blend of creativity and technical knowledge he brings is hard to find.",
+        imgPath: "/images/client2.png",
+    },
+    {
+        name: "Marvin McKinney",
+        mentions: "@marvinmckinney",
+        review:
+            "Anant revamped our backend architecture with a clean, scalable approach that’s now powering our platform effortlessly. His ability to write efficient APIs and optimize performance has improved our entire development workflow.",
+        imgPath: "/images/client5.png",
+    },
+    {
+        name: "Floyd Miles",
+        mentions: "@floydmiles",
+        review:
+            "Anant is a full-stack powerhouse. He built our entire web application from the ground up, ensuring top-tier performance on the backend and a sleek, engaging frontend. His work significantly elevated our digital presence.",
+        imgPath: "/images/client4.png",
+    },
+    {
+        name: "Albert Flores",
+        mentions: "@albertflores",
+        review:
+            "Anant doesn’t just build websites—he crafts experiences. His deep understanding of both frontend and backend development, combined with a sharp design sense, gave us a product that’s not only functional but truly delightful to use.",
+        imgPath: "/images/client6.png",
+    },
+];
+
+const socialImgs = [
+    {
+        name: "insta",
+        url: "https://www.instagram.com/",
+        imgPath: "/images/insta.png",
+    },
+    {
+        name: "fb",
+        url: "https://www.facebook.com/",
+        imgPath: "/images/fb.png",
+    },
+    {
+        name: "x",
+        url: "https://www.x.com/",
+        imgPath: "/images/x.png",
+    },
+    {
+        name: "linkedin",
+        url: "https://www.linkedin.com/",
+        imgPath: "/images/linkedin.png",
+    },
+];
+
+export {
+    words,
+    abilities,
+    logoIconsList,
+    counterItems,
+    expCards,
+    expLogos,
+    testimonials,
+    socialImgs,
+    techStackIcons,
+    techStackImgs,
+    navLinks,
+};
