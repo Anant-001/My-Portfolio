89f8c599c152ed818368c37ceeb4ac3f98556a9b
Anant-001 <anantdevliyal01@gmail.com>
2026-05-24 17:08:03 +0530

    exoerience

--- src/components/LogoSection.jsx
+++ src/components/LogoSection.jsx
@@ -1,31 +1,32 @@
-import React from 'react'
-import {logoIconsList} from "../constants/index.js";
+import React from "react";
+import { logoIconsList } from "../constants/index.js";
 
-const LogoIcon =({icon}) =>{
-    return(
+const LogoIcon = ({ icon }) => {
+    return (
         <div className="flex-none flex-center marquee-item">
-            <img src={icon.imgPath} alt={icon.name}/>
+            <img src={icon.imgPath} alt={icon.name} loading="lazy" />
         </div>
-    )
-}
+    );
+};
 
 const LogoSection = () => {
+    // duplicate list for seamless marquee
+    const repeatedLogos = [...logoIconsList, ...logoIconsList];
+
     return (
         <div className="md:my-20 my-10 relative">
-            <div className="gradient-edge"/>
-            <div className="gradient-edge"/>
+            <div className="gradient-edge" />
+            <div className="gradient-edge" />
+
             <div className="marquee h-52">
                 <div className="marquee-box md:gap-12 gap-5">
-                    {logoIconsList.map((icon) =>(
-                        <LogoIcon Key={icon.name} icon={icon} />
-                    ))}
-
-                    {logoIconsList.map((icon) =>(
-                        <LogoIcon Key={icon.name} icon={icon} />
+                    {repeatedLogos.map((icon, index) => (
+                        <LogoIcon key={`${icon.name}-${index}`} icon={icon} />
                     ))}
                 </div>
             </div>
         </div>
-    )
-}
-export default LogoSection
+    );
+};
+
+export default LogoSection;
